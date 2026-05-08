6304bdf95be9ecf3687ec8a5e54858bb26e34934
Suryansh0301 <159011111+Suryansh0301@users.noreply.github.com>
2026-05-08 17:11:48 +0000

    added comments

--- internal/response/response.go
+++ internal/response/response.go
@@ -2,6 +2,7 @@ package response
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"http-from-tcp/constants"
 	"http-from-tcp/enums"
@@ -10,7 +11,10 @@ import (
 	"strconv"
 )
 
-func (W *Writer) GetDefaultHeaders(contentLen int) headers.Headers {
+// GetDefaultHeaders returns a standard set of HTTP response headers.
+// It sets Content-Length to the provided contentLen, Connection to "close",
+// and Content-Type to plain text.
+func (w *Writer) GetDefaultHeaders(contentLen int) headers.Headers {
 	headers := headers.NewHeaders()
 
 	headers.Set(constants.ContentLength, strconv.Itoa(contentLen))
@@ -29,6 +33,9 @@ func NewWriter(conn io.Writer) *Writer {
 	}
 }
 
+// WriteStatusLine writes the HTTP/1.1 status line for the given status code.
+// For example: "HTTP/1.1 200 OK\r\n"
+// Returns an error if the status code is not supported.
 func (w *Writer) WriteStatusLine(statusCode enums.StatusCode) error {
 	var statusLine []byte
 	switch statusCode {
@@ -39,7 +46,7 @@ func (w *Writer) WriteStatusLine(statusCode enums.StatusCode) error {
 	case enums.StatusCodeInternalServerError:
 		statusLine = []byte("HTTP/1.1 500 Internal Server Error" + constants.CRLF)
 	default:
-		return fmt.Errorf("invalid error code")
+		return errors.New("invalid error code")
 	}
 
 	_, err := w.writer.Write(statusLine)
@@ -50,6 +57,9 @@ func (w *Writer) WriteStatusLine(statusCode enums.StatusCode) error {
 	return nil
 }
 
+// WriteHeaders writes all HTTP headers followed by the terminal CRLF that
+// separates headers from the response body.
+// Each header is written as "Key: Value\r\n", ending with an extra "\r\n".
 func (w *Writer) WriteHeaders(headers headers.Headers) error {
 	byteHeaders := make([]byte, 0)
 	for headerKey := range headers {
@@ -65,6 +75,8 @@ func (w *Writer) WriteHeaders(headers headers.Headers) error {
 	return nil
 }
 
+// WriteBody writes raw bytes directly to the underlying writer.
+// Returns the number of bytes written and any error encountered.
 func (w *Writer) WriteBody(p []byte) (int, error) {
 	n, err := w.writer.Write(p)
 	if err != nil {
@@ -73,6 +85,14 @@ func (w *Writer) WriteBody(p []byte) (int, error) {
 	return n, nil
 }
 
+// WriteChunkedBody writes a single chunk in HTTP chunked transfer encoding format.
+// Each chunk is written as:
+//
+//	<hex size>\r\n
+//	<data>\r\n
+//
+// Null bytes are trimmed from p before writing to handle fixed-size buffers
+// that may not be fully filled.
 func (w *Writer) WriteChunkedBody(p []byte) (int, error) {
 	var size int
 	n, err := w.WriteBody([]byte(fmt.Sprintf("%x\r\n", len(bytes.TrimRight(p, "\x00")))))
@@ -93,6 +113,9 @@ func (w *Writer) WriteChunkedBody(p []byte) (int, error) {
 	return size, nil
 }
 
+// WriteChunkedBodyDone writes the terminal chunk that signals the end of a
+// chunked transfer encoded response body.
+// The terminal chunk is always "0\r\n" per the HTTP/1.1 spec.
 func (w *Writer) WriteChunkedBodyDone() (int, error) {
 	n, err := w.WriteBody([]byte("0\r\n"))
 	if err != nil {
@@ -101,6 +124,9 @@ func (w *Writer) WriteChunkedBodyDone() (int, error) {
 	return n, nil
 }
 
+// WriteTrailers writes trailing headers after the final chunk in a chunked
+// transfer encoded response. Trailers follow the same "Key: Value\r\n" format
+// as regular headers and must be declared in the Trailer header beforehand.
 func (w *Writer) WriteTrailers(h headers.Headers) error {
 	return w.WriteHeaders(h)
 }
